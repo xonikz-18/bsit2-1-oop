e6975422ba2d1fdeb9382b912fa83355ba569d92
Nikki U. Pacatang <npacatang89487@liceo.edu.ph>
2025-08-01 11:25:41 +0800

    lab-acitivity-2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
+    static int num = 0;
+
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+
+        System.out.println("INPUT FIVE NUMBERS");
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter Number " + i + ": " );
+            num = num + scanner.nextInt();
+        }
+        System.out.println(" ");
+        System.out.println("Total Number: " + num);
+
+        scanner.close();
     }
 }
