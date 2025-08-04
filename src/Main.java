e973bbea2707ab043541d52829e95429167ba3e4
Nikki Pacatang <npacatang89487@liceo.edu.ph>
2025-08-04 12:36:06 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,65 @@
 public class Main {
-    static String title = "";
-    static String author = "";
-    static int pages = "";
-    boolean isAvailable = "true";
+    public static void main(String[] args) {
+        Book book1 = new Book("Accel World, Vol. 1 : Kuroyukihime Return", "Reki Kawahara", 232);
+        Book book2 = new Book("Komi Cant Communicate, Vol. 1", "Tomohito Oda", 192);
+        Book book3 = new Book("Death Note Black Edition, Vol. 1", "Tsugumi Ohba", 400);
 
-    public Book(String bookTitle, String bookAuthor, int bookPages) {
-        title = bookTitle;
-        author = bookAuthor;
-        pages = bookPages;
-        System.out.println("Main Branch!");
+        System.out.println();
+        book1.displayInfo();
+        book1.borrowBook();
+        System.out.println();
+        book1.displayInfo();
+        book1.returnBook();
+        System.out.println();
+        book1.displayInfo();
+
+        System.out.println();
+        book2.displayInfo();
+        book2.borrowBook();
+        System.out.println();
+        book2.displayInfo();
+        book2.returnBook();
+        System.out.println();
+        book2.displayInfo();
+
+        System.out.println();
+        book3.displayInfo();
+        book3.borrowBook();
+        System.out.println();
+        book3.displayInfo();
+        book3.returnBook();
+        System.out.println();
+        book3.displayInfo();
+    }
+}
+
+class Book {
+    String title;
+    String author;
+    int pages;
+    boolean isAvailable = true;
+
+    Book(String t, String a, int p) {
+        title = t;
+        author = a;
+        pages = p;
+        System.out.println("A new book '" + title + "' by " + author + " has been added to the library!");
+    }
+
+    void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Pages: " + pages);
+        System.out.println("Available: " + (isAvailable ? "Yes" : "No"));
+    }
+
+    void borrowBook() {
+        isAvailable = false;
+        System.out.println("You have borrowed '" + title + "'.");
+    }
+
+    void returnBook() {
+        isAvailable = true;
+        System.out.println("You have returned '" + title + "'.");
     }
 }
