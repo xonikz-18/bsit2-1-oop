6551e0116d6d799105f57256b42a8b1c3beb3738
Nikki Pacatang <npacatang89487@liceo.edu.ph>
2025-09-02 22:59:07 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        LibraryManager manager = new LibraryManager();
+        System.out.println("≡ Library Management System ≡");
+
+        manager.showBooks();
+        manager.addBook();
+        manager.addBook();
+        manager.addBook();
+        manager.removeBook();
+        manager.removeBook();
+        manager.removeBook();
+
+        System.out.println("\nProgram completed successfully.");
     }
 }
