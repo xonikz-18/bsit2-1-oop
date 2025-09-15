c7c1b3f7b429f660b84e1ba87fcedd8b31717fca
Nikki Pacatang <npacatang89487@liceo.edu.ph>
2025-09-15 10:22:29 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         PetService service = new PetService();
-
+        
         System.out.println("Basic Checkup: $" + service.calculateFee());
         System.out.println("Checkup with Vaccination: $" + service.calculateFee(true));
         System.out.println("Full Service : $" + service.calculateFee(true, true));
         System.out.println("Emergency Service: $" + service.calculateFee("emergency"));
 
-        // Part 2: Runtime Polymorphism
         System.out.println("\nWelcome to the Pet Clinic!");
         System.out.println("=================================");
         Pet dog = new Dog("Buddy", 3);
@@ -24,7 +23,6 @@ public class Main {
         trainPet((Trainable) bird);
     }
 
-    // Accepts any object that implements Trainable
     public static void trainPet(Trainable pet) {
         pet.performTrick();
     }
